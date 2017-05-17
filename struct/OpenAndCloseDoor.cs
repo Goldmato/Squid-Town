1d28dd0887c890bae31e88c2abbf232aba25e25b
Goldmato <sam.berrada@gmail.com>
2017-05-17 11:40:31 -0600

    Initial commit

---
+++ struct/OpenAndCloseDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAndCloseDoor : MonoBehaviour
+{
+
+	public GameObject door;
+
+	void OpenDoor ()
+	{
+		door.SetActive (false);
+	}
+
+	void CloseDoor ()
+	{
+		door.SetActive (true);
+	}
+}
