22b0d8d4842070e6af42009a78efacc1f3eb76e4
Goldmato <sam.berrada@gmail.com>
2017-05-16 20:32:10 -0600

    Initial commit

---
+++ cont/managers/EnemyManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager
+{
+	// TODO: Implement features shown in https://www.draw.io/#DEnemyManager
+}
