632436cc1c572febbf8afed77312b7c6f486772f
Goldmato <sam.berrada@gmail.com>
2017-05-19 22:05:24 -0600

    better singleton handling

--- shared/Registry.cs
+++ shared/Registry.cs
@@ -15,13 +15,26 @@ public class Registry : MonoBehaviour
     public List<Enemy> Enemies = new List<Enemy>();
 
     private static Registry m_Instance;
+    private static bool m_ExceptionFlag;
 
     void Awake()
     {
         if(m_Instance == null)
             m_Instance = this;
-        else
-            throw new UnityException("Please ensure there is only one Registry in the scene");
+        else if(!m_ExceptionFlag)
+        {
+            m_ExceptionFlag = true;
+            string activeRegistries = "GameObject(s) with Registry component(s)\n" +
+                                      "--------------------------------------\n";
+            foreach(var reg in GameObject.FindObjectsOfType(typeof(Registry))) 
+            {
+                activeRegistries += "\"" + reg.name + "\"\n";
+            }
+            
+            Debug.Break();
+            Debug.LogError(activeRegistries);
+            throw new UnityException("Please ensure there is only one active registry in the scene");
+        }
     }
 
     void Start()
