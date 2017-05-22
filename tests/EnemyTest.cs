726a3dc366c5578959ecd6c5559ca2856b0a72f0
Goldmato <sam.berrada@gmail.com>
2017-05-22 13:57:00 -0600

    changed algorithm to go to the nearest door

--- tests/EnemyTest.cs
+++ tests/EnemyTest.cs
@@ -9,43 +9,56 @@ public class EnemyTest : MonoBehaviour
     [HideInInspector] public bool Continue = true;
 
 	[SerializeField] NavMeshAgent m_Agent;
-    [SerializeField] GameObject m_Plane;
+/*    [SerializeField] GameObject m_Plane;
 
-    [SerializeField] [Range(5f, 10f)] float m_PathFindDelay = 5f;
+    [SerializeField] [Range(5f, 10f)] float m_PathFindDelay = 5f;*/
 
     Vector3 m_OriginalPos;
-    Vector3 m_PlaneSize;
+/*    Vector3 m_PlaneSize;
     Vector3 m_PlanePos;
-    Vector2 m_PlaneOffset;
+    Vector2 m_PlaneOffset;*/
 
     void Start()
     {
         m_OriginalPos = transform.position;
-        m_PlanePos = m_Plane.transform.position;
+/*        m_PlanePos = m_Plane.transform.position;
         m_PlaneSize = m_Plane.GetComponent<Collider>().bounds.size;
-        m_PlaneOffset = new Vector2(m_PlanePos.x + (m_PlaneSize.x / 2), m_PlanePos.z + (m_PlaneSize.z / 2));
+        m_PlaneOffset = new Vector2(m_PlanePos.x + (m_PlaneSize.x / 2), m_PlanePos.z + (m_PlaneSize.z / 2));*/
         StartCoroutine(GoToAllDoors());
     }
 
+    ///<summary>
+    /// Goes to every door starting with the nearest
+    ///</summary>
     IEnumerator GoToAllDoors()
     {
         var doors = Registry.Current.Doors;
 
         Debug.Log(gameObject.name + " enemy beginning door pathfinding routine...");
 
-        for (int i = 0; i < doors.Count; i++)
+        while(doors.Count > 0)
         {
-            Continue = false;
+            float minDist = float.MaxValue;
+            int selectedDoor = 0;
 
-            m_Agent.destination = doors[i].transform.position;
-            do 
+            for (int i = 0; i < doors.Count; i++)
             {
-                yield return null;
-            } while (!Continue);
+                if(Vector3.Distance(transform.position, doors[i].transform.position) < minDist)
+                {
+                    selectedDoor = i;
+                }
+            }
 
-            yield return new WaitForSeconds(1);
-            Debug.Log("Door[" + i + "] succesfully reached, moving on to next target...");
+            // Set selected doors as target destination and
+            // remove from list
+            m_Agent.destination = doors[selectedDoor].transform.position;
+            doors.RemoveAt(selectedDoor);
+
+            // Pause execution
+            Continue = false;
+            while(!Continue) { yield return null; }
         }
+        Debug.Log("All doors successfully reached! Returning to starting position...");
 
         m_Agent.destination = m_OriginalPos;
         while (m_Agent.remainingDistance > 0.1f)
