4dae918831a408e7d329adbe44871f310f77dfd4
Goldmato <sam.berrada@gmail.com>
2017-05-24 22:02:06 -0600

    Initial commit

---
+++ npc/behaviours/DoorMagnet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.AI;
+
+///<summary>
+/// Enemy movement behaviour that targets the nearest
+/// ddor while ignoring the previous 2 doors visited
+/// while using this behaviour
+///</summary>
+public class DoorMagnet : MoveBehaviour
+{
+    private int[] m_DoorsToIgnore = { -1, -1 };
+    private int m_DoorIgnoreIndex;
+
+    public void Move(NavMeshAgent agent, Vector3 currentPos)
+    {
+        var doors = Registry.Current.Doors;
+
+        float minDist = float.MaxValue;
+        int selectedDoor = -1;
+
+        for(int i = 0; i < doors.Count; i++)
+        {
+            if(IsDoorIgnored(i))
+                continue;
+
+            float dist = Vector3.Distance(currentPos, doors[i].transform.position);
+
+            if(dist < minDist)
+            {
+                selectedDoor = i;
+                minDist = dist;
+            }
+        }
+
+        m_DoorsToIgnore[m_DoorIgnoreIndex++] = selectedDoor;
+        if(m_DoorIgnoreIndex == m_DoorsToIgnore.Length)
+            m_DoorIgnoreIndex = 0;
+
+        if(selectedDoor >= 0)
+            agent.destination = doors[selectedDoor].transform.position;
+        else
+            throw new UnityException("ERROR No door selected");
+    }
+
+    bool IsDoorIgnored(int index)
+    {
+        for (int i = 0; i < m_DoorsToIgnore.Length; i++)
+        {
+            if(m_DoorsToIgnore[i] == index)
+                return true;
+        }
+        return false;
+    }
+}
---
+++ shared/EnemyInterfaces.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public interface MoveBehaviour
+{
+    void Move(NavMeshAgent agent, Vector3 currentPos);
+}
