dd64d45f87999c9861c656f65a55b71a4add4685
Goldmato <sam.berrada@gmail.com>
2017-05-27 11:34:13 -0600

    implemented enemy jail spawn method * moved debugging/testing code to gamecontroller * misc formatting

--- cont/sub/EnemySpawner.cs
+++ cont/sub/EnemySpawner.cs
@@ -18,72 +18,80 @@ public class EnemySpawner : MonoBehaviour
     private GameObject m_Container;
 
     void Start()
-	{
+    {
         m_Container = new GameObject("Enemies");
-
-        // DEBUGGING/TESTING
-        SpawnEnemies(SpawnMethod.InHouses);
     }
 
     public void SpawnEnemies(SpawnMethod method, int numEnemies = 10)
-	{
-		switch (method)
-		{
-			case SpawnMethod.Random:
+    {
+        switch(method)
+        {
+            case SpawnMethod.Random:
                 SpawnEnemiesRandom(numEnemies);
                 break;
             case SpawnMethod.InHouses:
-				SpawnEnemiesInHouses();
+                SpawnEnemiesInHouses();
                 break;
             case SpawnMethod.InJail:
-				SpawnEnemiesInJail(numEnemies);
+                SpawnEnemiesInJail(numEnemies);
                 break;
             default:
-                throw new UnityException("ERROR Invalid spawn method requested (EnemyManager)");
+                throw new UnityException("Invalid spawn method requested (EnemyManager)");
         }
+    }
 
-        EnemyController.Current.MoveAllEnemies();
-	}
-
-	void SpawnEnemiesRandom(int numEnemies)
-	{
+    void SpawnEnemiesRandom(int numEnemies)
+    {
         Debug.Log("Spawning [" + numEnemies + "] enemies randomly");
 
         float terrainRadius = (m_Terrain.terrainData.size.x + m_Terrain.terrainData.size.z) / 2;
 
-        for (int i = 0; i < numEnemies; i++)
-		{
+        for(int i = 0; i < numEnemies; i++)
+        {
             NavMeshHit hit;
             Vector3 randomPos = new Vector3(Random.Range(m_Terrain.transform.position.x, m_Terrain.terrainData.size.x + m_Terrain.transform.position.x), 0,
-										    Random.Range(m_Terrain.transform.position.z, m_Terrain.transform.position.z + m_Terrain.terrainData.size.z));
+                                            Random.Range(m_Terrain.transform.position.z, m_Terrain.transform.position.z + m_Terrain.terrainData.size.z));
             NavMesh.SamplePosition(randomPos, out hit, terrainRadius, NavMesh.AllAreas);
 
             var newEnemy = Instantiate(m_SpawnableEnemies[Random.Range(0, m_SpawnableEnemies.Count)],
-				 hit.position, Quaternion.identity) as Enemy;
+                 hit.position, Quaternion.identity) as Enemy;
             newEnemy.transform.SetParent(m_Container.transform);
 
             // Debug.Log("Moving enemy [" + i + "] to nearest door...");
         }
     }
 
-	void SpawnEnemiesInHouses()
-	{
+    void SpawnEnemiesInHouses()
+    {
         Debug.Log("Spawning [" + Registry.Current.Doors.Count + "] enemies in houses");
-        
-        for (int i = 0; i < Registry.Current.Doors.Count; i++)
+
+        for(int i = 0; i < Registry.Current.Doors.Count; i++)
         {
             var newEnemy = Instantiate(m_SpawnableEnemies[Random.Range(0, m_SpawnableEnemies.Count)],
-				 Registry.Current.Doors[i].transform.position, Quaternion.identity) as Enemy;
+                 Registry.Current.Doors[i].transform.position, Quaternion.identity) as Enemy;
             newEnemy.transform.SetParent(m_Container.transform);
-
-            newEnemy.Move();
         }
-	}
+    }
 
-	void SpawnEnemiesInJail(int numEnemies)
-	{
+    void SpawnEnemiesInJail(int numEnemies)
+    {
         Debug.Log("Spawning [" + numEnemies + "] enemies in jail");
-        
-        throw new System.NotImplementedException();
+
+        Transform jailTrans = GameController.Current.Jail.transform;
+        Collider barCol = jailTrans.Find("bars").GetComponent<Collider>();
+
+        for (int i = 0; i < numEnemies; i++)
+        {
+            //TODO: Spawn enemies in a circular area within the main jail
+
+            float barRadius = (barCol.bounds.extents.x + barCol.bounds.extents.z) / 2;
+            Vector2 circlePos = Random.insideUnitCircle * barRadius;
+            Vector3 randomPos = new Vector3(circlePos.x, 0, circlePos.y) + barCol.transform.position;
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomPos, out hit, barRadius, NavMesh.AllAreas);
+
+            var newEnemy = Instantiate(m_SpawnableEnemies[Random.Range(0, m_SpawnableEnemies.Count)],
+                 hit.position, Quaternion.identity) as Enemy;
+        }
     }
 }
