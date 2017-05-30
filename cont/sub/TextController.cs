31f52412dd8ad635560763551920986fec4e6cd2
Goldmato <sam.berrada@gmail.com>
2017-05-30 11:00:16 -0600

    simple text controller script for controlling various text elements (i.e. the score text)

---
+++ cont/sub/TextController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    [SerializeField] private Text m_ScoreText;
+
+    public void UpdateScore(int newScore)
+    {
+        if(newScore > 999 || newScore < 0) { throw new UnityException("Score out of range (TextController)"); }
+
+        m_ScoreText.text = newScore.ToString();
+    }
+}
