2eb60b7623fa6586730bf98937adf6b996d8a24f
Goldmato <sam.berrada@gmail.com>
2017-05-30 10:08:28 -0600

    simple code to check if occupied

--- struct/Door.cs
+++ struct/Door.cs
@@ -8,7 +8,20 @@ using UnityEngine.AI;
 /// Class controlling door behaviour/data
 ///</summary>
 [RequireComponent(typeof(Collider))]
-public class Door : MonoBehaviour 
+public class Door : MonoBehaviour
 {
-    
+    private bool m_IsOccupied;
+
+    public bool Occupy()
+    {
+        if(m_IsOccupied)
+            return false;
+        m_IsOccupied = true;
+        return true;
+    }
+
+    public void Leave()
+    {
+        m_IsOccupied = false;
+    }
 }
