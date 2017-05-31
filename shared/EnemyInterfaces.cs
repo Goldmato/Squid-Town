9209317fc3500a3cb98944e7af2bf1f8bf8e1cf0
Goldmato <sam.berrada@gmail.com>
2017-05-31 14:49:32 -0600

    converted from simple interface into an abstract class * includes base final method for running from the player * stores reference to enemy

--- shared/EnemyInterfaces.cs
+++ shared/EnemyInterfaces.cs
@@ -4,7 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public interface EnemyMoveBehaviour
+///<summary>
+/// Abstract interface for all Enemy movement behaviour classes
+///</summary>
+public abstract class EnemyMoveBehaviour
 {
-    void MoveNext(NavMeshAgent agent, Vector3 currentPos);
+    protected Enemy m_Enemy;
+
+    public EnemyMoveBehaviour(Enemy enemy)
+    {
+        m_Enemy = enemy;
+    }
+
+    public abstract void MoveNext();
+
+    public void RunFromPlayer(float distance = 25f)
+    {
+        // TODO: Find angle between player and enemy and send enemy
+        // a random distance in that direction
+        Debug.Log("Running from player!");
+
+        var player = GameController.Current.Player;
+
+        Vector3 direction = (m_Enemy.transform.position - player.transform.position).normalized;
+
+        m_Enemy.Agent.destination = m_Enemy.transform.position + (direction * distance);
+    }
 }
