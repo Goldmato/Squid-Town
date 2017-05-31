9b6522ca6f2c41dc28cc3d7062a899e38441b63d
Goldmato <sam.berrada@gmail.com>
2017-05-31 10:46:40 -0600

    renamed movebehaviour interface

--- shared/EnemyInterfaces.cs
+++ shared/EnemyInterfaces.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public interface MoveBehaviour
+public interface EnemyMoveBehaviour
 {
     void MoveNext(NavMeshAgent agent, Vector3 currentPos);
 }
