ca1324301d887cbc1132d286f0472cf3370d798a
Goldmato <sam.berrada@gmail.com>
2017-05-31 14:45:29 -0600

    simple script for toggling enemy run state

---
+++ player/TriggerEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class TriggerEnemy : MonoBehaviour 
+{
+    void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            var enemy = other.GetComponent<Enemy>();
+            if(!enemy.RunState)
+                enemy.RunState = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            var enemy = other.GetComponent<Enemy>();
+            enemy.RunState = false;
+        }
+    }
+}
