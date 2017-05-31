3b4f1008af06c8404d3151df86b3bbe33aa9f120
Goldmato <sam.berrada@gmail.com>
2017-05-31 10:46:25 -0600

    now stores edge location

--- struct/Door.cs
+++ struct/Door.cs
@@ -10,7 +10,16 @@ using UnityEngine.AI;
 [RequireComponent(typeof(Collider))]
 public class Door : MonoBehaviour
 {
+    public Vector3 Edge { get { return m_DoorEdge; } }
+    public bool Occupied { get { return m_IsOccupied; } }
+
+    private Vector3 m_DoorEdge;
     private bool m_IsOccupied;
+
+    void Awake()
+    {
+        m_DoorEdge = transform.position + (-transform.up * GetComponent<Collider>().bounds.extents.y);
+    }
 
     public bool Occupy()
     {
