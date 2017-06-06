29dfa337ad7feec3abc9e42605320376b559c808
Goldmato <sam.berrada@gmail.com>
2017-06-06 11:17:51 -0600

    Initial commit (new behaviour script for squid enemies)

---
+++ npc/base_enemy/SquidEnemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+///<summary>
+/// Base squid enemy behaviour
+///</summary>
+public class SquidEnemy : Enemy 
+{
+	// TODO: Implement squid-only behaviours
+}
